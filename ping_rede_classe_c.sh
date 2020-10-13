080cb448c1de734441025d4619d2fe2c84ed9df1
Elton <eltonpduarte@gmail.com>
2020-10-13 00:05:06 -0300

    Update ping_rede_classe_c.sh

--- ping_rede_classe_c.sh
+++ ping_rede_classe_c.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 for i in {1..5};do
- ping -c1 "$1.$i" | grep "icmp_seq=1" | awk -F " " '{print $4}' | sed 's/.$//' 
+  ping -c1 "$1.$i" | grep "icmp_seq=1" | awk -F " " '{print $4}' | sed 's/.$//' 
 done
